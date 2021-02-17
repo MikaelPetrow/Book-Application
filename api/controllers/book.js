ea1cc4ad8d2d87b7e0e197fb6e25375b6c230203
MikaelPetrow <mikael.petrow@gmail.com>
2021-02-17 23:52:09 +0200

    added crud, database connection, migrations and seeders

--- api/controllers/book.js
+++ api/controllers/book.js
@@ -1,5 +1,5 @@
 const db = require('../models')
-const book = db.Book
+const Book = db.Book
 
 module.exports = {
   test: async(req, res) => {
@@ -16,22 +16,50 @@ module.exports = {
   },
 
   list: async(req, res) => {
-    
+    let books = await Book.findAll({})
+    res.json(books)
   },
 
   get: async(req, res) => {
-    
+    const { id } = req.params
+    const book = await Book.findOne({
+      where: { id: id }
+    })
+    res.json(book)
   },
 
   create: async(req, res) => {
-    
+    const { title, author, description } = req.body
+    const newBook = await Book.create({
+      title: title,
+      author: author,
+      description: description
+    })
+    const book = await Book.findOne({
+      where: { id: newBook.id }
+    })
+    res.json(book)
   },
 
   update: async(req, res) => {
-    
+    const { title, author, description } = req.body
+    const { id } = req.params
+    await Book.update({
+      title: title,
+      author: author,
+      description: description
+    }, { where: { id: id } })
+    const book = await Book.findOne({
+      where: { id: id }
+    })
+    res.json(book)
   },
 
   delete: async(req, res) => {
-    
+    const { id } = req.params
+    await Book.destroy({
+      where: { id: id }
+    })
+    res.json({message: 'deleted'})
   }
 }
--- api/database/seeders/20210217203445-book.js
+++ api/database/seeders/20210217203445-book.js
@@ -2,10 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
+    await queryInterface.bulkInsert('book', [
+      {
+        title: 'Book 1',
+        author: 'Author 1',
+        description: 'This is the desctiption for Book 1',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Book 2',
+        author: 'Author 2',
+        description: 'This is the desctiption for Book 2',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {})
   },
 
   down: async (queryInterface, Sequelize) => {
-    
+    await queryInterface.bulkDelete('book', null, {});
   }
 };
